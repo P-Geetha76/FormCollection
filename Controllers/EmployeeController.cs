77d6dc71eec5d4601209f350722418078fc10d44
P-Geetha <98796315+P-Geetha@users.noreply.github.com>
2022-02-01 11:36:42 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using FormCollection.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormCollection.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            MVC_DBEntities1 dbContext = new MVC_DBEntities1();
+            List<Employee> empList = dbContext.Employees.ToList();
+            return View(empList);
+        }
+    }
+}
